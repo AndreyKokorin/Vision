bc7860376289df66ae3fbd8cffcb9211eac9b5a4
AndreyKokorin <andrey.kokorin.007@gmail.com>
2023-06-02 08:34:14 +0600

    second

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const canvas = document.querySelector(".glass");
 const context = canvas.getContext("2d");
 const main = document.querySelector(".main");
-
+const lastic = document.querySelector(".lastic")
+const eraseText = document.querySelector(".erase");
+const aftertext = document.querySelector(".aftertext");
+const afterBtn = document.querySelector(".afterbtn");
 
 const image = new Image();
-image.src ="img/glass.png";
+image.src ="img/glass.svg";
 
 image.onload = function() {
     context.drawImage(image, 0, 0, 300, 130);
@@ -13,29 +16,45 @@ image.onload = function() {
 
 
 const mouseMoveListener = (event) => {
-    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     if(event.buttons === 1){
-        canvas.classList.add("pouseAnim")
+        canvas.classList.add("pouseAnim");
+
         var rect = canvas.getBoundingClientRect();
         var x = event.clientX - rect.left;
         var y = event.clientY - rect.top;
+
         context.clearRect(x - 15, y - 26.5, 53, 50);
-    }
-    
-    let isTransparent = true;
-    for (var i = 3; i < imageData.data.length; i += 4) {
-        var alpha = imageData.data[i + 3];
-        if (alpha !== 0) {
-          isTransparent = false;
-          break;
-        }
+
+        lastic.style.top = event.clientY - 25  + "px";
+        lastic.style.left = event.clientX - 26 + "px";
+
+        lastic.classList.remove("lasticOff");
+        lastic.classList.add("lasticShow");
+        canvas.classList.add("cursorN");
+
+        eraseText.classList.add("opacity0")
     }
 
-    if(isTransparent){
-        console.log("Стерт полностью")
+    if(isFullyTransparent()){
+        main.classList.add("smile");
+        aftertext.classList.add("aftertextAnim");
+        afterBtn.classList.add("opacity1");
+        lastic.remove();
     }
 }
 
-imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+function isFullyTransparent() {
+    var d = context.getImageData(0, 0, canvas.width, canvas.height).data;
+    for (var i=3; i < d.length; i+=4)
+    if (d[i]) 
+      return false
+    return true
+  }
+  
+canvas.addEventListener("mouseup", () => {
+    lastic.classList.add("lasticOff")
+    lastic.classList.remove("lasticShow")
+    canvas.classList.remove("cursorN")
+})
 canvas.addEventListener("mousemove", mouseMoveListener)
 
